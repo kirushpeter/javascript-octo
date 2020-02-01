86521f5d030777e7866ea7da7bf9d73402f297ca
pip-peter <peterkir5693@outlook.com>
2020-02-01 14:48:27 +0300

    first commit

---
+++ datatypes.js
@@ -0,0 +1,46 @@
+
+console.log("hello world this is javascript")
+ 
+var number = 5; // in-line comment
+
+console.log(number)
+
+/*this is a multi-line comment*/
+
+var sum = 10 + 10;
+
+console.log(sum)
+
+var difference = 40 - 30;
+
+console.log(difference)
+
+var myvar = 10;
+
+myvar ++;
+
+console.log(myvar)
+
+var myvar = 11;
+
+myvar --;
+
+console.log(myvar)
+
+var product = 2.0 * 4.9;
+
+console.log(product)
+
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+//only modified code below this line
+
+console.log(a = a + 12)
+b = 9 + b;
+console.log(c += 13)
